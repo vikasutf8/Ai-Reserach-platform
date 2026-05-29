e31ae98ea3f72ec28cb127aa36bc79175f6e36b7
Vikas Arya <117626243+vikasutf8@users.noreply.github.com>
2026-05-30 01:55:05 +0530

    setup project structure

--- src/test/java/com/springai/airesearchagent/AiResearchAgentApplicationTests.java
+++ src/test/java/com/springai/airesearchagent/AiResearchAgentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AiResearchAgentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
